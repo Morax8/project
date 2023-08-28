d14dba7f2ddaa4f58669243cb097dbb4216a683d
Morax8 <adityazianurragman@gmail.com>
2023-08-28 09:55:34 +0700

    eskul contoller

--- app/Http/Controllers/EskulController.php
+++ app/Http/Controllers/EskulController.php
@@ -20,25 +20,6 @@ class EskulController extends Controller
         return view('home.eskul', compact('footer'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
     public function show(eskul $eskul)
     {
         //
