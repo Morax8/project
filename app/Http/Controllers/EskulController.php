4cc30d867b1cfb56a99e68a1b65946c01e2b783a
Morax8 <adityazianurragman@gmail.com>
2023-09-11 10:36:52 +0700

    adding eskul cms

--- app/Http/Controllers/EskulController.php
+++ app/Http/Controllers/EskulController.php
@@ -19,6 +19,57 @@ class EskulController extends Controller
         return view('home.eskul', compact('footer', 'eskul'));
     }
 
+    public function futsal()
+    {
+        $eskul = eskul::where('type', 'futsal')->get();
+        $footer = Footer::firstorFail();
+        return view('home.eskuls', compact('eskul', 'footer'));
+    }
+
+    public function multimedia()
+    {
+        $eskul = eskul::where('type', 'mm')->get();
+        $footer = Footer::firstorFail();
+        return view('home.eskuls', compact('eskul', 'footer'));
+    }
+    public function band()
+    {
+        $eskul = eskul::where('type', 'band')->get();
+        $footer = Footer::firstorFail();
+        return view('home.eskuls', compact('eskul', 'footer'));
+    }
+    public function paskib()
+    {
+        $eskul = eskul::where('type', 'paskib')->get();
+        $footer = Footer::firstorFail();
+        return view('home.eskuls', compact('eskul', 'footer'));
+    }
+    public function pramuka()
+    {
+        $eskul = eskul::where('type', 'pramuka')->get();
+        $footer = Footer::firstorFail();
+        return view('home.eskuls', compact('eskul', 'footer'));
+    }
+    public function badminton()
+    {
+        $eskul = eskul::where('type', 'badmin')->get();
+        $footer = Footer::firstorFail();
+        return view('home.eskuls', compact('eskul', 'footer'));
+    }
+    public function tenismeja()
+    {
+        $eskul = eskul::where('type', 'tenismeja')->get();
+        $footer = Footer::firstorFail();
+        return view('home.eskuls', compact('eskul', 'footer'));
+    }
+    public function padus()
+    {
+        $eskul = eskul::where('type', 'padus')->get();
+        $footer = Footer::firstorFail();
+        return view('home.eskuls', compact('eskul', 'footer'));
+    }
+
+
     public function show(eskul $eskul)
     {
         $eskul = eskul::firstorFail();
@@ -37,26 +88,35 @@ class EskulController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, eskul $eskul, $type, $id)
+    public function update(Request $request, $type, $id)
     {
+        // Validate the request data
         $request->validate([
             'title' => 'required',
             'content' => 'required',
         ]);
 
-        $content = eskul::find($id);
+        // Find the content by its ID
+        $content = Eskul::find($id);
+
         if (!$content) {
-            return redirect()->back();
+            return redirect("/{$type}cms")->with('error', 'Content not found.');
         }
 
-
-        if ($type == 'main' || $type == 'futsal') {
+        // Check the $type and update the content accordingly
+        if ($type == 'eskul' || $type == 'futsal') {
             $content->update([
-                'title' => $request->title,
-                'content' => $request->content,
+                'title' => $request->input('title'),
+                'content' => $request->input('content'),
             ]);
+
+            // Redirect with a success message
+            return redirect("/{$type}cms")->with('success', 'Content updated successfully.');
+        } else {
+            return redirect("/{$type}cms")->with('error', 'Invalid content type.');
         }
     }
+
 
     /**
      * Remove the specified resource from storage.
