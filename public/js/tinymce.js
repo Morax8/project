1e461ff0fccada204f78b1230fb144a1392056d6
Morax8 <adityazianurragman@gmail.com>
2023-09-11 10:30:55 +0700

    adding more plugins

--- public/js/tinymce.js
+++ public/js/tinymce.js
@@ -5,35 +5,21 @@ tinymce.init({
         "searchreplace visualblocks code fullscreen",
         "insertdatetime media table paste code help wordcount",
         "image",
+        "link",
+        "lists",
     ],
+
     images_upload_url: "/upload/image",
     toolbar:
         "undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | link image |help",
     content_style: "body { font-family: Arial, sans-serif; font-size: 14px; }",
-    /* enable title field in the Image dialog*/
     image_title: true,
-    /* enable automatic uploads of images represented by blob or data URIs*/
-    automatic_uploads: true,
-    /*
-  URL of our upload handler (for more details check: https://www.tiny.cloud/docs/configure/file-image-upload/#images_upload_url)
-  images_upload_url: 'postAcceptor.php',    
-  here we add custom filepicker only to Image dialog
-*/
     file_picker_types: "image",
     /* and here's our custom image picker*/
     file_picker_callback: function (cb, value, meta) {
         var input = document.createElement("input");
         input.setAttribute("type", "file");
         input.setAttribute("accept", "image/*");
-
-        /*
-    Note: In modern browsers input[type="file"] is functional without
-    even adding it to the DOM, but that might not be the case in some older
-    or quirky browsers like IE, so you might want to add it to the DOM
-    just in case, and visually hide it. And do not forget do remove it
-    once you do not need it anymore.
-  */
-
         input.onchange = function () {
             var file = this.files[0];
 
