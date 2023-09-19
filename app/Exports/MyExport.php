89b5b353ddc7300191303fd5dda7c85cff7a39ba
Morax8 <adityazianurragman@gmail.com>
2023-09-19 10:03:10 +0700

    adding export feature into xslx (maatwebsite/excel

---
+++ app/Exports/MyExport.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\ppdb;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+
+
+
+class MyExport implements FromCollection, WithHeadings, ShouldAutoSize, WithStyles // Add WithStyles
+{
+    public function headings(): array
+    {
+        return [
+            'ID',
+            'Jurusan',
+            'Mengetahui PPDB',
+            'Kerabat',
+            'Nama',
+            'NIK',
+            'Jenis Kelamin',
+            'Tempat Lahir',
+            'Tanggal Lahir',
+            'Agama',
+            'Domisili',
+            'RT/RW',
+            'Kelurahan',
+            'Kecamatan',
+            'Kota',
+            'Anak ke Berapa',
+            'Status Rumah',
+            'Nomor HP',
+            'Akun Sosial Media',
+            'Tinggi Badan',
+            'Berat Badan',
+            'Asal Sekolah',
+            'Kota Asal Sekolah',
+            'NISN',
+            'Tanggal Lulus',
+            'Ekstrakurikuler',
+            'Nama Ayah',
+            'NIK Ayah',
+            'Nomor KK',
+            'Tempat Lahir Ayah',
+            'Tanggal Lahir Ayah',
+            'Pendidikan Ayah',
+            'Pekerjaan Ayah',
+            'Nama Ibu',
+            'NIK Ibu',
+            'Tempat Lahir Ibu',
+            'Tanggal Lahir Ibu',
+            'Pendidikan Ibu',
+            'Pekerjaan Ibu',
+            'Mata Pelajaran Disukai',
+            'Tinggal Kelas',
+            'Prestasi',
+            'Gambar Lingkungan',
+            'Kartu Jakarta Pintar (KJP)',
+            'Program Indonesia Pintar (PIP)',
+            'Created At',
+            'Updated At',
+        ];
+    }
+
+    public function collection()
+    {
+        return ppdb::all(); // Fetch the data you want to export
+    }
+    public function styles(Worksheet $sheet)
+    {
+
+        // Style the header row (all columns) with a yellow background color
+        $sheet->getStyle('1')->applyFromArray([
+            'fill' => [
+                'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                'startColor' => [
+                    'rgb' => 'FFFF00', // Yellow color
+                ],
+            ],
+        ]);
+
+        // Style the data rows with borders
+        $lastRow = $sheet->getHighestRow();
+        $sheet->getStyle('2:' . $lastRow)->applyFromArray([
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                    'color' => ['argb' => '000000'], // Black color
+                ],
+            ],
+        ]);
+
+        // Style the entire worksheet to justify (center) the data
+        $sheet->getStyle($sheet->calculateWorksheetDimension())->applyFromArray([
+            'alignment' => [
+                'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                'vertical' => \PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER,
+            ],
+        ]);
+
+        return [
+            // Style the second column (B) to have a background color of yellow
+            1 => ['font' => ['bold' => true]],
+        ];
+    }
+}
--- config/app.php
+++ config/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Providers\AppServiceProvider;
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\ServiceProvider;
 
@@ -168,6 +169,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
     ])->toArray(),
 
     /*
@@ -183,6 +185,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
     ])->toArray(),
 
 ];
